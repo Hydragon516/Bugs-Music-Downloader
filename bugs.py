fe1b26ff0358b58a68ac321ff2c3b69742bc8289
Min Hyeok Lee <hydragon516@yonsei.ac.kr>
2021-08-13 18:45:54 +0900

    버그 수정

--- bugs.py
+++ bugs.py
@@ -8,6 +8,7 @@ import eyed3
 import os
 import shutil
 from selenium import webdriver
+from urllib import request
 
 search_title = ""
 
@@ -66,6 +67,7 @@ class MyMain(MyMainGUI):
 
         self.th_search = searcher(parent=self)
         self.th_search.updated_list.connect(self.list_update)
+        self.th_search.updated_label.connect(self.status_update)
 
         self.th_download = downloadr(parent=self)
         self.th_download.updated_label.connect(self.status_update)
@@ -100,6 +102,7 @@ class MyMain(MyMainGUI):
 
 class searcher(QThread):
     updated_list = pyqtSignal(str)
+    updated_label = pyqtSignal(str)
 
     def __init__(self, parent=None):
         super().__init__()
@@ -118,34 +121,58 @@ class searcher(QThread):
         album_list = []
         
         if search_title != "":
-            html = requests.get('https://music.bugs.co.kr/search/integrated?q=' + search_title)
-            soup = BeautifulSoup(html.text, 'html.parser')
+            self.updated_label.emit("서버에 접속하는 중...")
+
+            options = webdriver.ChromeOptions()
+            options.add_argument('--headless')
+            options.add_argument("--disable-gpu")
+            options.add_experimental_option('excludeSwitches', ['enable-logging'])
+            options.add_argument('--log-level=3')
+
+            driver = webdriver.Chrome('chromedriver', options=options)
+            driver.implicitly_wait(5)
+
+            driver.get(url='https://music.bugs.co.kr/search/track?q=' + search_title)
+
+            self.updated_label.emit("제목 정보를 불러오는 중...")
+
+            for indx in range(30):
+                try:
+                    target = driver.find_element_by_xpath('//*[@id="DEFAULT0"]/table/tbody/tr[{}]/th/p/a'.format(indx + 1))
+                    title_list.append(target.get_attribute("title"))
+                except:
+                    break
             
-            title = soup.find_all('p', {'class':'title'})
-            artist = soup.find_all('p', {'class':'artist'})
-            album = soup.find_all('a', {'class':'album'})
+            self.updated_label.emit("아티스트 정보를 불러오는 중...")
+            
+            for indx in range(30):
+                try:
+                    target = driver.find_element_by_xpath('//*[@id="DEFAULT0"]/table/tbody/tr[{}]/td[4]/p/a'.format(indx + 1))
+                    artist_list.append(target.get_attribute("title"))
+                except:
+                    break
+            
+            self.updated_label.emit("앨범 정보를 불러오는 중...")
+            
+            for indx in range(30):
+                try:
+                    target = driver.find_element_by_xpath('//*[@id="DEFAULT0"]/table/tbody/tr[{}]/td[5]/a'.format(indx + 1))
+                    album_list.append(target.get_attribute("title"))
+                except:
+                    break
 
-            if len(title) > 5:
-                album = album[1:]
-                
-                for i in range(len(album)):
-                    result = re.search('" title="(.*)">', str(album[i]))
-                    album_list.append(str(result.group(1)))
-
-                for i in range(len(title)):
-                    result = re.search('" title="(.*)">', str(title[i]))
-                    title_list.append(str(result.group(1)))
-
-                for i in range(len(artist)):
-                    result = re.search('" title="(.*)">', str(artist[i]))
-                    artist_list.append(str(result.group(1)))
-
-                title_list = title_list[:10]
-                artist_list = artist_list[:10]
-                album_list = album_list[:10]
-
+            try:
                 for i in range(len(artist_list)):
-                    self.updated_list.emit("%s // %s // %s" % (title_list[i], artist_list[i], album_list[i]))
+                    self.updated_list.emit("%s // %s // %s // %s" % (str(i + 1), title_list[i], artist_list[i], album_list[i]))
+            except:
+                pass
+
+            driver.close()
+
+            self.updated_label.emit("불러오기 완료!")
+        
+        else:
+            self.updated_label.emit("검색어를 입력하세요")
 
 
 class downloadr(QThread):
@@ -160,14 +187,66 @@ class downloadr(QThread):
 
     def run(self):
         global keyword
+        global search_title
 
-        self.updated_label.emit("음원 파일을 읽는 중 입니다...")
+        self.updated_label.emit("음원 파일을 읽는 중 ...")
         
-        target_title = keyword.split(" // ")[0]
-        target_artist = keyword.split(" // ")[1]
-        target_album = keyword.split(" // ")[2]
+        target_index = keyword.split(" // ")[0]
+        target_title = keyword.split(" // ")[1]
+        target_artist = keyword.split(" // ")[2]
+        target_album = keyword.split(" // ")[3]
 
-        surch_keyword = target_title + "-" + target_artist + " 음원 듣기"
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        options.add_argument("--disable-gpu")
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        options.add_argument('--log-level=3')
+
+
+        driver = webdriver.Chrome('chromedriver', options=options)
+        driver.implicitly_wait(5)
+
+        driver.get(url='https://music.bugs.co.kr/search/track?q=' + search_title)
+        target_music_button = driver.find_element_by_xpath('//*[@id="DEFAULT0"]/table/tbody/tr[{}]/td[3]/a'.format(target_index))
+        target_music_button.click()
+        
+        lyrics_url = driver.current_url
+
+        driver.close()
+
+        html = requests.get(lyrics_url)
+        soup = BeautifulSoup(html.text, 'html.parser')
+
+        cover = soup.find_all('div', {'class':'photos'})
+        cover_link = re.search('src="(.*)"/>', str(cover)).group(1)
+        request.urlretrieve(cover_link, "cover.jpg")
+
+        lyric = soup.find_all('div', {'class':'lyricsContainer'})
+        lines = str(lyric).split("\n")
+
+        line_state = False
+        lyrics = []
+
+        for item in lines:
+            if line_state == True:
+                if "</xmp></p>" not in item:
+                    lyrics.append(item)
+            
+            if "<p><xmp>" in item:
+                lyrics.append(item.replace("<p><xmp>", ""))
+                line_state = True
+            
+            if "</xmp></p>" in item:
+                lyrics.append(item.replace("</xmp></p>", ""))
+                break
+
+        with open("lyric.txt", 'w') as f:
+            for row in lyrics:
+                f.write(row)
+
+        f.close()
+
+        surch_keyword = target_title + " " + target_artist + " 음원"
 
         new_name = target_title + "_" + target_artist + ".mp3"
 
@@ -180,7 +259,6 @@ class downloadr(QThread):
         options.add_argument("--disable-gpu")
 
         driver = webdriver.Chrome('./chromedriver.exe', options=options)
-        
         
         driver.get(url)
         soup = BeautifulSoup(driver.page_source, 'html.parser')
@@ -213,12 +291,17 @@ class downloadr(QThread):
             if ".mp3" in file:
                 os.rename(file, new_name)
         
+        OpenLyircsFile = open("lyric.txt", 'r') 
+        ReadLyirsFile = OpenLyircsFile.read() 
+
         audiofile = eyed3.load("./" + new_name)
         audiofile.initTag()
         audiofile.tag.artist = target_artist
         audiofile.tag.title = target_title
         audiofile.tag.album = target_album
-        audiofile.tag.save()
+        audiofile.tag.lyrics.set(ReadLyirsFile)
+        audiofile.tag.images.set(3, open('cover.jpg','rb').read(), 'image/jpeg')
+        audiofile.tag.save(version=eyed3.id3.ID3_V2_3)
 
         if not os.path.exists("./변환된 파일"):
             os.makedirs("./변환된 파일")
@@ -226,6 +309,7 @@ class downloadr(QThread):
         shutil.move(new_name, "./변환된 파일/" + new_name)
 
         self.updated_label.emit("변환 완료!")
+
 
 
 if __name__ == "__main__":
